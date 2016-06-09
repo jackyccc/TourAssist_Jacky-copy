6b4eb0a45cec6e657aa98b80f01aa3835a886c27
Chee Chong Cheah <jackyccc@chees-macbook-pro.local>
2016-06-09 17:53:46 -0400

    Initial Commit

---
+++ TourAssist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TourAssist
+//
+//  Created by Chee Chong Cheah on 6/9/16.
+//  Copyright © 2016 Chee Chong Cheah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
